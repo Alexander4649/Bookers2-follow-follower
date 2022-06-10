b0cc19474dfc3d7f4c77de59e952ea89c1114c19
Alexander4649 <105344687+Alexander4649@users.noreply.github.com>
2022-06-10 15:11:45 +0000

    [Finish]RSpecテスト終了=リンクエラーのみ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,26 @@
 class BooksController < ApplicationController
+  before_action :ensure_correct_book, only: [:update,:edit]
 
   def show
-    @book = Book.find(params[:id])
+    @book = Book.new
+    @book_id = Book.find(params[:id])
+    @user = @book_id.user
   end
 
   def index
+    @book = Book.new
+    @user = User.find(current_user.id)
     @books = Book.all
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    @user = User.find(current_user.id)
+    @books = Book.all
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
-      @books = Book.all
       render 'index'
     end
   end
@@ -32,15 +38,24 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title,:body)
   end
+  
+   def ensure_correct_book
+     @book = Book.find(params[:id])
+     @user = @book.user
+   if
+      @user != current_user
+      redirect_to books_path
+   end
+   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  belongs_to :books
+  has_many :books,dependent: :destroy
   has_one_attached :profile_image
 
-  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true,presence: true
+  validates :introduction, length: { maximum: 50 }
 
-  
+  has_one_attached :profile_image
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
